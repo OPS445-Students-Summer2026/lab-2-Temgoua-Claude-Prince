c041d2ace20b8a07e5dc8534f3f0d4a051f8b71e
Claude Prince Temgoua Zambou <cptemgoua-zambou@myseneca.ca>
2026-05-22 22:29:34 -0400

    Completed Lab 2

---
+++ lab2e.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+# Author: CLAUDE PRINCE TEMGOUA ZAMBOU
+# Author ID: cptemgoua-zambou
+# Date Created: 2026/05/22
+
+timer = 10
+
+while timer != 0:
+    print(timer)
+    timer = timer - 1
+
+print('blast off!')
